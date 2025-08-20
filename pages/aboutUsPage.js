8ef3e289cc54ca02a988a8e1d7676aaa16c85196
AasthaKaushik <aastha.kaushik@gspann.com>
2025-08-20 19:03:15 +0530

    Added Changes in about us page

--- pages/aboutUsPage.js
+++ pages/aboutUsPage.js
@@ -2,7 +2,7 @@ export class AboutUsPage {
   constructor(page) {
     this.page = page;
     this.url = "https://sauce-demo.myshopify.com/pages/about-us";
-    this.homeBreadcrumb = page.locator("//span[@itemprop='title']");
+    this.homeBreadcrumb = page.locator("//span[@itemprop='title']"); 
   }
 
   async navigateToAboutUs() {
