19392ecb144a995a91486f4c4cbe9a4a5dd6dd1c
AasthaKaushik <aastha.kaushik@gspann.com>
2025-08-21 10:49:00 +0530

    Added test cases hor headers links visibility and navigation

--- pages/homePage.js
+++ pages/homePage.js
@@ -7,6 +7,16 @@ export class HomePage {
     this.searchResults = page.locator('.product-grid h3');
     this.body = page.locator('body');
      this.noSearchMessage = page.locator('text=No search performed');
+    
+      this.headerLinks = {
+      'Search': page.getByRole('banner').getByRole('link', { name: 'Search' }),
+      'About Us': page.getByRole('banner').getByRole('link', { name: 'About Us' }),
+      'Log In': page.getByRole('link', { name: 'Log In' }),
+      'Sign Up': page.getByRole('link', { name: 'Sign up' }),
+      'My Cart': page.getByRole('link', { name: 'My Cart (0)' }),
+      'Check Out': page.getByRole('link', { name: 'Check Out' }),
+    };
+     
   }
 
   async navigateToHome() {
@@ -37,5 +47,19 @@ export class HomePage {
     } catch {
       return false;
     }
+  }
+
+  async isHeaderLinkVisible(linkName) {
+    const link = this.headerLinks[linkName];
+    return await link.isVisible();
+}
+
+ async clickHeaderLink(linkName) {
+    const link = this.headerLinks[linkName];
+    await link.click();
+}
+
+  async clickSearchBox() {
+    await this.searchBox.click();
   }
 }
