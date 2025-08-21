3a4300a92c6e4f391d132e80e104a9647d56438b
AasthaKaushik <aastha.kaushik@gspann.com>
2025-08-21 07:17:00 +0530

    Added Search Bar Test Case

--- pages/homePage.js
+++ pages/homePage.js
@@ -3,6 +3,10 @@ export class HomePage {
     this.page = page;
     this.homeUrl = 'https://sauce-demo.myshopify.com/';
     this.pageTitle = 'Sauce Demo';
+    this.searchBox = page.locator('#search-field');
+    this.searchResults = page.locator('.product-grid h3');
+    this.body = page.locator('body');
+    this.noResultsMessage = page.locator('text=/No results/i');
   }
 
   async navigateToHome() {
@@ -15,5 +19,18 @@ export class HomePage {
 
   async getPageTitle() {
     return await this.page.title();
+  }
+
+  async searchFor(query) {
+    await this.searchBox.fill(query);
+    await this.searchBox.press('Enter');
+  }
+
+  async hasResults() {
+    return await this.searchResults.count() > 0;
+  }
+
+  async isNoResultsVisible() {
+    return await this.noResultsMessage.isVisible();
   }
 }
