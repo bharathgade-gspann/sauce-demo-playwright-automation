45ab3e78ffd6f1c1b7ace8579947c42ce69f9774
bharathgade-gspann <bharath.gade@gspann.com>
2025-08-21 23:35:56 +0530

    commited jhenkins config

--- test/productDetails.spec.js
+++ test/productDetails.spec.js
@@ -7,7 +7,7 @@ test.describe('Product Details Page', () => {
   test('User should see correct product info when clicking on Black heels', async ({ catalogPage }) => {
     await catalogPage.goto();
     await catalogPage.clickCatalog();
-    await page.locator('#product-1').click();
+    await page.locator('#product-1').click(); 
     await catalogPage.verifyProductDetails('Black heels', '£45.00');
   });
 });
